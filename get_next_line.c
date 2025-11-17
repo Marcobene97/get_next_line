32142dd4127a690e0355d4fc40e88b96e796215c
Marco Benedettelli <mbenedet@3-F-13.42heilbronn.de>
2025-11-17 20:18:47 +0100

    initial commit

---
+++ get_next_line.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbenedet <mbenedet@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/17 17:52:54 by mbenedet          #+#    #+#             */
+/*   Updated: 2025/11/17 18:21:46 by mbenedet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char *get_next_line(int fd)
+{
+	
+
+	
+	/* 
+	Useful:
+	Functions:
+	read()
+	Prototype:
+	ssize_t read(int fildes, void*buf, size_t nbyte);
+
+	what does double s mean?
+	*/
