1c09e246f657017e0335c02c952927e99867354f
Marco Benedettelli <mbenedet@3-G-10.42heilbronn.de>
2025-12-15 15:58:34 +0100

    final changes eval ready

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: mbenedet <mbenedet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/17 17:52:59 by mbenedet          #+#    #+#             */
-/*   Updated: 2025/12/15 09:52:41 by mbenedet         ###   ########.fr       */
+/*   Updated: 2025/12/15 15:55:31 by mbenedet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,13 @@
 # include <fcntl.h>
 
 char	*get_next_line(int fd);
-size_t	ft_strlen_gnl(const char *str);
-size_t	ft_strlcpy(char *dest, const char *src, size_t dstsize);
+size_t	ft_strlen_gnl(const char *string);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 char	*ft_strchr(const char *s, int c);
-char	*ft_strdup(const char *src);
-char	*ft_strndup(const char *src, int len);
+char	*ft_strdup(const char *s);
+char	*ft_strjoin_free(char *s1, char *s2);
+void	extract_leftover(char *line, size_t linelen, char *stash);
+char	*extract_line(char *line, char *stash);
 
 #endif
